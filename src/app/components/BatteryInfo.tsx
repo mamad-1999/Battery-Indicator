91dc16337be0cc72c0d1a37ae9619d997536f847
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-12-25 22:01:46 +0330

    style: some change in battery style

--- src/app/components/BatteryInfo.tsx
+++ src/app/components/BatteryInfo.tsx
@@ -10,8 +10,8 @@ const BatteryInfo = ({ level, height }: BatteryInfoProps) => {
         <>
             <p className="mb-2 text-lg font-light">Battery Level</p>
             <h3 className="text-7xl font-bold mb-14">{level ? level! : 0}%</h3><div className="flex items-center justify-center relative mb-14">
-                <div className="w-[160px] h-[288px] border-8 border-gray-700 rounded-3xl relative before:absolute before:w-14 before:h-4 before:bg-gray-700 before:top-[-27px] before:left-[42px] before:rounded-md"></div>
-                <div style={{ height: `${height}px` }} className={`w-[133px] rounded-xl z-10 absolute bottom-3 ${(level < 40 && level >= 20) ? "bg-yellow-500" : (level < 20 && level >= 0) ? "bg-red-500" : "bg-lime-600"}`}></div>
+                <div className="w-[160px] h-[288px] border-[6px] border-slate-400 rounded-3xl relative before:absolute before:w-16 before:h-4 before:bg-slate-400 before:top-[-25px] before:left-[42px] before:rounded-md"></div>
+                <div style={{ height: `${height}px` }} className={`w-[133px] rounded-xl z-10 absolute bottom-3 ${(level < 40 && level >= 20) ? "bg-yellow-500" : (level < 20 && level >= 0) ? "bg-red-500" : "bg-gradient-to-r from-lime-400 from-5% via-lime-500 via-50% to-lime-600 to-90% "}`}></div>
             </div >
         </>
     );
