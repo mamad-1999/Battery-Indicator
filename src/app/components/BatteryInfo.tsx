33f786c169daed4a26a3982e5f868764680bb2d9
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-12-26 14:45:44 +0330

    chore: add svg {isCharging} and animation

--- src/app/components/BatteryInfo.tsx
+++ src/app/components/BatteryInfo.tsx
@@ -32,8 +32,13 @@ const BatteryInfo = ({ level, height, isCharging }: BatteryInfoProps) => {
                                 RED_CHARGE :
                                 GREEN_CHARGE}`}>
                 </div>
-            </div >
-            {isCharging ? <p className="mb-14">Charging...</p> : null}
+            </div>
+            {isCharging ?
+                <div className="flex justify-center items-center gap-2 mb-14">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-battery-charging w-5 h-5 animate-pulse stroke-lime-600"><path d="M5 18H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h3.19M15 6h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-3.19"></path><line x1="23" y1="13" x2="23" y2="11"></line><polyline points="11 6 7 12 13 12 9 18"></polyline></svg>
+                    <p className="font-light">Charging...</p>
+                </div>
+                : null}
         </>
     );
 };
